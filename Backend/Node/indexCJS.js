1d1226d4864a18c07a225cac4e92269ec0477cb4
wmvj <warleyjunior05@gmail.com>
2023-09-18 19:58:48 -0300

    Primeiro exemplo - Node CJS e ESM

---
+++ Backend/Node/indexCJS.js
@@ -0,0 +1,6 @@
+// primeira opção
+
+const operacoes = require('./operacoesCJS.js')
+
+const soma = operacoes.soma(4,2)
+console.log(soma)
---
+++ Backend/Node/operacoesCJS.js
@@ -0,0 +1,33 @@
+// // primeira opção
+
+exports.soma = function (num1, num2){
+    return num1 + num2
+}
+
+exports.multiplicar = function (num1, num2){
+    return num1 * num2
+}
+
+// // segunda opção
+
+// module.exports = {
+//     soma(num1, num2){
+//         return num1 + num2
+//     },
+//     multiplicar(num1, num2){
+//         return num1 * num2
+//     }
+// }
+
+// terceira opção
+
+// function soma(num1, num2){
+//     return num1 + num2
+// }
+
+// function subtrai(num1, num2){
+//     return num1 - num2
+// }
+
+// module.exports = soma
+// module.exports = {soma, subtrai}
