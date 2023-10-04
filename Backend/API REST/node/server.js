7cafa49d90203953030448867f2cf1bb0ad0b50c
wmvj <warleyjunior05@gmail.com>
2023-10-04 15:18:40 -0300

    Sexto exemplo de express

--- Backend/API REST/node/server.js
+++ Backend/API REST/node/server.js
@@ -2,7 +2,6 @@ import app from "./src/app.js"
 
 const port = 3000
 
-
 app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
--- Backend/API REST/node/src/app.js
+++ Backend/API REST/node/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express()
+app.use(express.json())
 
 const livros = [
     {id:1, titulo: "Harry Potter"},
@@ -15,5 +16,33 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
     res.status(200).json(livros)
 })
+
+app.get("/livros/:id", (req, res) => {
+    let index = buscaLivros(req.params.id)
+    res.status(200).json(livros[index])  
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body)
+    res.status(201).send("Este livro foi adicionado")
+})
+
+app.put("/livros/:id", (req, res) => {
+  let index = buscaLivros(req.params.id)
+  livros[index].titulo = req.body.titulo
+  res.status(200).json(livros)  
+})
+
+
+app.delete("/livros/:id", (req, res) => {
+    let index = buscaLivros(req.params.id)
+    livros.splice(index, 1)
+    res.send(`Livro ${id} foi removido`) 
+  })
+
+
+function buscaLivros(id){
+    return livros.findIndex(livro => livro.id === Number(id))
+}
 
 export default app
