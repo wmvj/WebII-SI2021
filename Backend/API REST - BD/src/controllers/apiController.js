8b1f9da0bc4c07fd80f632acf009f203ba69ad73
wmvj <warleyjunior05@gmail.com>
2023-10-18 13:39:30 -0300

    Exemplo de API com BD e Sequelize

--- Backend/API REST - BD/src/controllers/apiController.js
+++ Backend/API REST - BD/src/controllers/apiController.js
@@ -4,3 +4,13 @@ export const listPhrases = async (req, res) => {
     let list = await Phrase.findAll()
     res.json({list})
 }
+
+export const getPhrases = async (req, res) => {
+    let {id} = req.params
+    let phrase = await Phrase.findByPk(id)
+    if(phrase){
+        res.json({phrase})
+    } else {
+        res.json({erro: 'frase não foi encontrada'})
+    }
+}
--- Backend/API REST - BD/src/routes/api.js
+++ Backend/API REST - BD/src/routes/api.js
@@ -4,5 +4,7 @@ import * as apiController from "../controllers/apiController.js"
 const router = Router()
 
 router.get("/frases",apiController.listPhrases)
+router.get("/frases/:id",apiController.getPhrases)
+
 
 export default router
