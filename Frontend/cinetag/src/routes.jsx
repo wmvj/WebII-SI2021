9647dbc7a2f73176753f7e2160b6a278c9b4f087
wmvj <warleyjunior05@gmail.com>
2023-11-22 13:50:38 -0300

    Primeira versão front-end com react

---
+++ Frontend/cinetag/src/routes.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Inicio from "./pages/Inicio";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function AppRoutes() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Inicio />}></Route>
+        </Routes>
+      </BrowserRouter>
+    );
+}
+
+export default AppRoutes;
